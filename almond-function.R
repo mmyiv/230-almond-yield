83ca298b51fe95061df41578116703098defdf90
Jorb1 <baileyjorgensen101@gmail.com>
2025-04-10 13:03:06 -0700

    work on function

---
+++ almond-function.R
@@ -0,0 +1,6 @@
+almond_model <- function(data, Tn, Tx, P) {
+  # Transfer function from Lobell et al. 2006
+  Y <- -0.015(Tn of month2) -0.0046(Tn of month2)^2 - 0.07(P of month1) = 0.0043(P of month1)^2
+  # Return calculated yield anomaly
+  return(Y)
+}
